adc537eacd88eedb5da9ec9177c03d2c38649ffe
Evan Inge <einge823@gmail.com>
2025-03-07 23:40:07 -0500

    handle overlapping events

--- src/services/event-services.ts
+++ src/services/event-services.ts
@@ -1,6 +1,6 @@
 import { eventsTable } from "../db/schema.js";
 import { db } from "../index.js";
-import { eq } from "drizzle-orm";
+import { and, or, eq, gte, lte } from "drizzle-orm";
 import type { CreateEventRequest } from "../types/create-event-request.js";
 
 export async function createEvent(event: CreateEventRequest) {
@@ -9,6 +9,38 @@ export async function createEvent(event: CreateEventRequest) {
         start: new Date(event.start),
         end: new Date(event.end),
     };
+
+    const eventsWithOverlap = await db
+    .select()
+    .from(eventsTable)
+    .where(
+      and(
+        eq(eventsTable.user_id, eventWithDates.user_id),
+        or(
+          and(
+            gte(eventsTable.start, eventWithDates.start),
+            lte(eventsTable.end, eventWithDates.end)
+          ),
+          and(
+            gte(eventsTable.start, eventWithDates.start),
+            lte(eventsTable.start, eventWithDates.end)
+          ),
+          and(
+            gte(eventsTable.end, eventWithDates.start),
+            lte(eventsTable.end, eventWithDates.end)
+          ),
+          and(
+            lte(eventsTable.start, eventWithDates.start),
+            gte(eventsTable.end, eventWithDates.end)
+          )
+        )
+      )
+    );
+
+    if (eventsWithOverlap.length > 0) {
+        throw new Error("Event overlap detected");
+    }
+
     const result = await db
         .insert(eventsTable)
         .values(eventWithDates)
